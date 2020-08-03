bc24a665f3c051c5444dc97a10c76ab93bc37212
Marcus Siegel <msiegel37@gmail.com>
2020-08-03 22:37:20 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,9 @@
 # Write your code here
 count = 0 
-while count < 10 && 
- count != 0 
+while 
+ count >= 1 
 do 
-  count= count - 1 
+ count -= 1 
 while count == 0 
  do 
    puts "Happy New Year"
